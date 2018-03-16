f4e452026653255fc33a68298fabaac97c1e5a11
Unknown <neil.mckimm@ucdconnect.ie>
2018-03-16 09:22:14 +0000

    Still not working
    Trying over and over again to fix the Apply function to correctly process the commands. I think the issue is either in the coordinates or else slicing the coordinates. I eventually got it working for 3, but not the others.

--- led_module/lights.py
+++ led_module/lights.py
@@ -20,29 +20,41 @@ class LightTester:
         N, instructions = parseFile(input)
         
         prac = np.copy(self.leds)
-        for i in range(len(instructions)):
-            print(instructions)
-            instruction = instructions[i] ## Tuple instruction changes for each loop (where i = line number in file)
-            command, x1, y1, x2, y2 = instruction ## Unpacking tuple and assigning each element
-            print(instruction)
+        for i in range(len(instructions)+1):
+            instruction = instructions[i-1] ## Tuple instruction changes for each loop (where i = line number in file)
+            command, y1, x1, y2, x2 = instruction ## Unpacking tuple and assigning each element
             x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
-            print(command, x1, y1, x2, y2)
+            y1, x1 = y1-1, x1-1
+            # if len x1 < N/10 or N/100 oe N=1000
+            #x1, x2, y1, y2 = x1-1, x2-1, y1-1, y2-1
             if (command == 'turn on'):
-                prac[x1:x2,y1:y2] = True
-                print(prac)
-                
+                print(instruction)                
+                prac[y1:y2,x1:x2] = True
+                print(np.sum(prac))
             elif (command == 'turn off'):
-                prac[x1:x2,y1:y2] = False
-                print(prac)
+                print(instruction)
+                prac[y1:y2,x1:x2] = False
+                print(np.sum(prac))
             elif (command == 'switch'):
-                prac[x1:x2,y1:y2] = np.logical_not(prac[x1:x2,y1:y2]) #opposite values for t/f
-                print(prac)
+                print(instruction)
+                if (y2 < y1) or (x2 < x1):
+                    x1, x2 = x2, x1
+                    y1, y2 = y2, y1
+                    prac[y1:y2,x1:x2] = np.logical_not(prac[y1:y2,x1:x2]) 
+                else:
+                    prac[y1:y2,x1:x2] = np.logical_not(prac[y1:y2,x1:x2]) #opposite values for t/f
+                print(np.sum(prac))
             else:
-                return 0
-        print(prac)        
+                continue
+            print(prac)
+        print(prac)
         print('#Occupied: ',np.sum(prac)) #prints number of True values (lights on)
-            
+        
+        
+        #4,4 6,6 interpreted as 4,6 4,6
+        
         #if cmd.search("switch"):
+        #is it parsing the file correctly? ignore line with incorrect spelling of word
 
 
     
